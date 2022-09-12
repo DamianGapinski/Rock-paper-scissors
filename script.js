ee8360aa4c980b116c342c0b1af4f2866c8aecee
Damian <71337183+DamianGapinski@users.noreply.github.com>
2022-09-12 23:51:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const paper = document.querySelector('.paper')
 const scissors = document.querySelector('.scissors')
 const rock = document.querySelector('.rock')
 
+const paperIcon = document.querySelector('.paperIcon')
+const scissorsIcon = document.querySelector('.scissorsIcon')
+const rockIcon = document.querySelector('.rockIcon')
+
 const choice = document.querySelector('.choice')
 const game = document.querySelector('.game')
 const result = document.querySelector('.result')
@@ -18,130 +22,147 @@ const housePick = document.querySelector('.housePick')
 let scoreNr = document.querySelector('.scoreNr')
 
 rulesClose.addEventListener('click', () => {
-    rules.style.display = 'none';
+  rules.style.display = 'none';
 })
 
 rulesButton.addEventListener('click', () => {
-    rules.style.display = 'flex'
+  rules.style.display = 'flex'
 })
 
 let score = 0;
 let userChoice = '';
-let choiceNr = null; 
+let choiceNr = null;
 
-  let SCORE = 0;
-  let shapeStyle = '';
+let SCORE = 0;
+let shapeStyle = '';
 
-  rock.addEventListener('click', () =>{
-    result.style.display = 'flex'
-    game.style.display = 'flex'
-    choice.style.display = 'none'
-    userChoice = 'rock'
-    shapeStyle = 'border: 15px solid red;'
-    selectshape(userChoice,shapeStyle)
-  })
-
-  paper.addEventListener('click', () =>{
-    result.style.display = 'flex'
-    game.style.display = 'flex'
-    choice.style.display = 'none'
-    userChoice = 'paper'
-    shapeStyle = 'border: 15px solid royalblue;'
-    selectshape(userChoice,shapeStyle)
-  })
-
-  scissors.addEventListener('click', () =>{
-    result.style.display = 'flex'
-    game.style.display = 'flex'
-    choice.style.display = 'none'
-    userChoice = 'scissors'
-    shapeStyle = 'border: 15px solid yellow;'
-    selectshape(userChoice,shapeStyle)
-
-  })
-
-  const selectshape = (userChoice,shapeStyle) =>{
-    userPick.innerHTML = `<img class="${userChoice}Icon" src="./images/icon-${userChoice}.svg">`
-    userPick.style = shapeStyle;
-    setTimeout(() =>{
-      housePick.style.backgroundColor = 'var(--light-color)'
-      aiPick(userChoice)    
-    },1000)
-    
-  }
-
-  const aiPick = (userChoice) =>{
-    
-    const possibilities = ['paper','scissors','rock']
-    let aiNr = Math.floor(Math.random() * 3)
-    housePick.innerHTML = `<img class="${possibilities[aiNr]}Icon" src="./images/icon-${possibilities[aiNr]}.svg">`
-
-    if(aiNr == 0) housePick.style.border = '15px solid royalblue'
-    else if(aiNr == 1) housePick.style.border = '15px solid yellow'
-    else housePick.style.border = '15px solid red'
-    gameResult(userChoice,aiNr)
-  }
-
-  const gameResult = (userChoice,aiNr) =>{
-    result.style.opacity = '1'
-
-    if(userChoice == 'paper' && aiNr == 0)
-    {
-        resultText.innerHTML = '<h1>DROW</h1>'
-        
-        
-    }
-    else if(userChoice == 'paper' && aiNr == 1){
-        resultText.innerHTML = '<h1>YOU LOSE</h1>'
-        score = score - 1;
-    }
-    else if(userChoice == 'paper' && aiNr == 2){
-        resultText.innerHTML = '<h1>YOU WIN</h1>'
-        score = score + 1;
-    }
-    else if(userChoice == 'scissors' && aiNr == 0){
-        resultText.innerHTML = '<h1>YOU WIN</h1>'
-        score = score + 1;
-    }
-    else if(userChoice == 'scissors' && aiNr == 1){
-        resultText.innerHTML = '<h1>DROW</h1>'
-    }
-    else if(userChoice == 'scissors' && aiNr == 2){
-        resultText.innerHTML = '<h1>YOU LOSE</h1>'
-        score = score - 1;
-    }
-    else if(userChoice == 'rock' && aiNr == 0){
-        resultText.innerHTML = '<h1>YOU LOSE</h1>'
-        score = score - 1;
-    }
-    else if(userChoice == 'rock' && aiNr == 1){
-        resultText.innerHTML = '<h1>YOU WIN</h1>'
-        score = score + 1;
-    }
-    else if(userChoice == 'rock' && aiNr == 2){
-        resultText.innerHTML = '<h1>DROW</h1>'
-    }
-    scoreNr.innerHTML = score
-  }
-
-    
-resultBtn.addEventListener('click', () =>{
-    result.style.opacity = '0'
-    result.style.display = 'none'
-    game.style.display = 'none'
-    choice.style.display = 'flex'
-    rulesButton.style.display = 'flex'
-    housePick.style.border = '15px solid transparent'
-    housePick.style.backgroundColor = 'transparent'
-    housePick.innerHTML = ''
-    userChoice = ''
-    aiNr = ''
-    resultText.innerHTML = ''
+rock.addEventListener('click', () => {
+  result.style.display = 'flex'
+  game.style.display = 'flex'
+  choice.style.display = 'none'
+  userChoice = 'rock'
+  shapeStyle = 'border: 15px solid red;'
+  rockIcon.style = 'width: 100px;'
+  rockIcon.style = 'height: 100px;'
+  selectshape(userChoice, shapeStyle)
 })
-  
-    
-    
-  
 
-  
+paper.addEventListener('click', () => {
+  result.style.display = 'flex'
+  game.style.display = 'flex'
+  choice.style.display = 'none'
+  userChoice = 'paper'
+  shapeStyle = 'border: 15px solid royalblue;'
+  paperIcon.style = 'width: 100px;'
+  paperIcon.style = 'height: 100px;'
+  selectshape(userChoice, shapeStyle)
+})
+
+scissors.addEventListener('click', () => {
+  result.style.display = 'flex'
+  game.style.display = 'flex'
+  choice.style.display = 'none'
+  userChoice = 'scissors'
+  shapeStyle = 'border: 15px solid yellow;'
+  scissorsIcon.style = 'width: 100px;'
+  scissorsIcon.style = 'height: 100px;'
+  selectshape(userChoice, shapeStyle)
+
+})
+
+const selectshape = (userChoice, shapeStyle) => {
+  userPick.innerHTML = `<img class="${userChoice}Icon" src="./images/icon-${userChoice}.svg">`
+  userPick.style = shapeStyle;
+  setTimeout(() => {
+    housePick.style.backgroundColor = 'var(--light-color)'
+    aiPick(userChoice)
+  }, 1000)
+
+}
+
+const aiPick = (userChoice) => {
+
+  const possibilities = ['paper', 'scissors', 'rock']
+  let aiNr = Math.floor(Math.random() * 3)
+  housePick.innerHTML = `<img class="${possibilities[aiNr]}Icon" src="./images/icon-${possibilities[aiNr]}.svg">`
+
+  if (aiNr == 0) {
+    housePick.style.border = '15px solid royalblue'
+    paperIcon.style = 'width: 100px;'
+    paperIcon.style = 'height: 100px;'
+  }
+  else if (aiNr == 1) {
+    housePick.style.border = '15px solid yellow'
+    scissorsIcon.style = 'width: 100px;'
+    scissorsIcon.style = 'height: 100px;'
+  }
+  else {
+    housePick.style.border = '15px solid red'
+    rockIcon.style = 'width: 100px;'
+    rockIcon.style = 'height: 100px;'
+  }
+  gameResult(userChoice, aiNr)
+}
+
+const gameResult = (userChoice, aiNr) => {
+  result.style.opacity = '1'
+
+  if (userChoice == 'paper' && aiNr == 0) {
+    resultText.innerHTML = '<h1>DROW</h1>'
+
+
+  }
+  else if (userChoice == 'paper' && aiNr == 1) {
+    resultText.innerHTML = '<h1>YOU LOSE</h1>'
+    score = score - 1;
+  }
+  else if (userChoice == 'paper' && aiNr == 2) {
+    resultText.innerHTML = '<h1>YOU WIN</h1>'
+    score = score + 1;
+  }
+  else if (userChoice == 'scissors' && aiNr == 0) {
+    resultText.innerHTML = '<h1>YOU WIN</h1>'
+    score = score + 1;
+  }
+  else if (userChoice == 'scissors' && aiNr == 1) {
+    resultText.innerHTML = '<h1>DROW</h1>'
+  }
+  else if (userChoice == 'scissors' && aiNr == 2) {
+    resultText.innerHTML = '<h1>YOU LOSE</h1>'
+    score = score - 1;
+  }
+  else if (userChoice == 'rock' && aiNr == 0) {
+    resultText.innerHTML = '<h1>YOU LOSE</h1>'
+    score = score - 1;
+  }
+  else if (userChoice == 'rock' && aiNr == 1) {
+    resultText.innerHTML = '<h1>YOU WIN</h1>'
+    score = score + 1;
+  }
+  else if (userChoice == 'rock' && aiNr == 2) {
+    resultText.innerHTML = '<h1>DROW</h1>'
+  }
+  scoreNr.innerHTML = score
+}
+
+
+resultBtn.addEventListener('click', () => {
+  result.style.opacity = '0'
+  result.style.display = 'none'
+  game.style.display = 'none'
+  choice.style.display = 'flex'
+  rulesButton.style.display = 'flex'
+  housePick.style.border = '15px solid transparent'
+  housePick.style.backgroundColor = 'transparent'
+  housePick.innerHTML = ''
+  userChoice = ''
+  aiNr = ''
+  resultText.innerHTML = ''
+})
+
+
+
+
+
+
 
